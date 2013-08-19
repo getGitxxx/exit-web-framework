4ea424cc184f853158d54a0bddf6fae7dd91e59a
exitsoft <es.chenxiaobo@gmail.com>
2013-08-19 14:39:38 +0800

    修改archetype-generate.bat文件，在本地生成模板。

--- showcase/basic-curd/bin/archetype-generate.bat
+++ showcase/basic-curd/bin/archetype-generate.bat
@@ -1,9 +1,12 @@
 @echo off
 echo [INFO] archetype create from project
 
-cd %~dp0
+if not exist generated-sources (md generated-sources)
+
+cd generated-sources
+
+call mvn archetype:generate -DarchetypeCatalog=local -DarchetypeGroupId=org.exitsoft.showcase -DarchetypeArtifactId=exitsoft-basic-curd-archetype -DarchetypeVersion=1.1.0
+
 cd ..
-call mvn archetype:generate -DarchetypeGroupId=org.exitsoft.showcase -DarchetypeArtifactId=exitsoft-basic-curd-archetype -DarchetypeVersion=1.1.0
-cd bin
 
 pause
