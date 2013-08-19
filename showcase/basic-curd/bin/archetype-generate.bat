31dccbc035aa171c8ac6b485b0065a86b3d9b5e6
exitsoft <es.chenxiaobo@gmail.com>
2013-08-19 09:43:42 +0800

    更新一些错误bat文件

--- showcase/basic-curd/bin/archetype-generate.bat
+++ showcase/basic-curd/bin/archetype-generate.bat
@@ -3,6 +3,7 @@ echo [INFO] archetype create from project
 
 cd %~dp0
 cd ..
-call mvn archetype:create-from-project
+call mvn archetype:generate -DarchetypeGroupId=org.exitsoft.showcase -DarchetypeArtifactId=exitsoft-basic-curd-archetype -DarchetypeVersion=1.1.0
 cd bin
+
 pause
