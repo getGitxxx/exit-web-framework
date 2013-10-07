e4650e951664c7e41f307f21eadf2473a6b3107c
exitsoft <es.chenxiaobo@gmail.com>
2013-10-07 11:55:31 +0800

    将创建表和插入数据库的sql分开。

--- showcase/basic-curd/src/main/resources/sql/h2/data.sql
+++ showcase/basic-curd/src/main/resources/sql/h2/data.sql
@@ -1,38 +1,4 @@
---删除所有表
-drop table tb_data_dictionary if exists;
-drop table tb_dictionary_category if exists;
-drop table tb_group if exists;
-drop table tb_group_resource if exists;
-drop table tb_group_user if exists;
-drop table tb_resource if exists;
-drop table tb_user if exists;
-
---创建系统字典表
-create table tb_data_dictionary (id varchar(32) not null, name varchar(256) not null, remark varchar(512), type varchar(1) not null, value varchar(32) not null, fk_category_id varchar(32) not null, primary key (id));
-create table tb_dictionary_category (id varchar(32) not null, code varchar(128) not null, name varchar(256) not null, remark varchar(512), fk_parent_id varchar(32), primary key (id));
-
---创建权限表
-create table tb_group (id varchar(32) not null, name varchar(32) not null, remark varchar(512), state integer not null, type varchar(2) not null, fk_parent_id varchar(32), role varchar(64), value varchar(256), primary key (id));
-create table tb_group_resource (fk_resource_id varchar(32) not null, fk_group_id varchar(32) not null);
-create table tb_group_user (fk_group_id varchar(32) not null, fk_user_id varchar(32) not null);
-create table tb_resource (id varchar(32) not null, permission varchar(64), remark varchar(512), sort integer not null, name varchar(32) not null, type varchar(2) not null, value varchar(256), fk_parent_id varchar(32), icon varchar(32), primary key (id));
-create table tb_user (id varchar(32) not null, email varchar(128), password varchar(32) not null, portrait varchar(32), realname varchar(64) not null, state integer not null, username varchar(32) not null, primary key (id));
-
---创建所有表关联
-alter table tb_dictionary_category add constraint UK_9qkei4dxobl1lm4oa0ys8c3nr unique (code);
-alter table tb_group add constraint UK_byw2jrrrxrueqimkmgj3o842j unique (name);
-alter table tb_resource add constraint UK_aunvlvm32xb4e6590jc9oooq unique (name);
-alter table tb_user add constraint UK_4wv83hfajry5tdoamn8wsqa6x unique (username);
-alter table tb_data_dictionary add constraint FK_layhfd1butuigsscgucmp2okd foreign key (fk_category_id) references tb_dictionary_category;
-alter table tb_dictionary_category add constraint FK_bernf41kympxy2kjl4vbq5q44 foreign key (fk_parent_id) references tb_dictionary_category;
-alter table tb_group add constraint FK_idve4hc50mytxm181wl1knw28 foreign key (fk_parent_id) references tb_group;
-alter table tb_group_resource add constraint FK_q82fpmfh128qxoeyymrkg71e2 foreign key (fk_group_id) references tb_group;
-alter table tb_group_resource add constraint FK_3tjs4wt3vvoibo1fvcvog5srd foreign key (fk_resource_id) references tb_resource;
-alter table tb_group_user add constraint FK_7k068ltfepa1q75qtmvxuawk foreign key (fk_user_id) references tb_user;
-alter table tb_group_user add constraint FK_rgmkki7dggfag6ow6eivljmwv foreign key (fk_group_id) references tb_group;
-alter table tb_resource add constraint FK_k2heqvi9muk4cjyyd53r9y37x foreign key (fk_parent_id) references tb_resource;
-
---插入字典列别数据
+--插入字典类别数据
 INSERT INTO `tb_dictionary_category` VALUES ('402881e437d467d80137d46fc0e50001', 'state', '状态', null, null);
 INSERT INTO `tb_dictionary_category` VALUES ('402881e437d467d80137d4709b9c0002', 'resource-type', '资源类型', null, null);
 INSERT INTO `tb_dictionary_category` VALUES ('402881e437d467d80137d4712ca70003', 'group-type', '组类型', null, null);
@@ -91,7 +57,7 @@ INSERT INTO `tb_user` VALUES ('17909124407b8d7901407be4996c0006', 'es.nick@es.co
 INSERT INTO `tb_user` VALUES ('17909124407b8d7901407be4996c0007', 'es.nick@es.com', 'e10adc3949ba59abbe56e057f20f883e', null, 'user3', '1', 'es004');
 INSERT INTO `tb_user` VALUES ('SJDK3849CKMS3849DJCK2039ZMSK0001', 'es.chenxiaobo@gmail.com', '21232f297a57a5a743894a0e4a801fc3', null, 'vincent.chen', '1', 'admin');
 
---插入中间表数据
+--插入组与资源中间表数据
 INSERT INTO `tb_group_resource` VALUES ('SJDK3849CKMS3849DJCK2039ZMSK0003', 'SJDK3849CKMS3849DJCK2039ZMSK0002');
 INSERT INTO `tb_group_resource` VALUES ('SJDK3849CKMS3849DJCK2039ZMSK0004', 'SJDK3849CKMS3849DJCK2039ZMSK0002');
 INSERT INTO `tb_group_resource` VALUES ('SJDK3849CKMS3849DJCK2039ZMSK0005', 'SJDK3849CKMS3849DJCK2039ZMSK0002');
@@ -131,6 +97,8 @@ INSERT INTO `tb_group_resource` VALUES ('SJDK3849CKMS3849DJCK2039ZMSK0022', '402
 INSERT INTO `tb_group_resource` VALUES ('SJDK3849CKMS3849DJCK2039ZMSK0023', '402881c4408c7d2301408c870ed10002');
 INSERT INTO `tb_group_resource` VALUES ('SJDK3849CKMS3849DJCK2039ZMSK0024', '402881c4408c7d2301408c870ed10002');
 INSERT INTO `tb_group_resource` VALUES ('SJDK3849CKMS3849DJCK2039ZMSK0025', '402881c4408c7d2301408c870ed10002');
+
+--插入组与用户中间表数据
 INSERT INTO `tb_group_user` VALUES ('SJDK3849CKMS3849DJCK2039ZMSK0002', 'SJDK3849CKMS3849DJCK2039ZMSK0001');
 INSERT INTO `tb_group_user` VALUES ('402881c4408c7d2301408c86b7a80001', '17909124407b8d7901407be4996c0005');
 INSERT INTO `tb_group_user` VALUES ('402881c4408c7d2301408c86b7a80001', '17909124407b8d7901407be4996c0006');
