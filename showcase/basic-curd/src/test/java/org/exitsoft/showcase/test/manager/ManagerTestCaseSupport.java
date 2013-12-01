48617be6574e621ba439bf401005324430d87796
exitsoft <es.chenxiaobo@gmail.com>
2013-12-01 13:35:22 +0800

    修改配置文件,使用spring profiles来划分环境.

--- showcase/basic-curd/src/test/java/org/exitsoft/showcase/test/manager/ManagerTestCaseSupport.java
+++ showcase/basic-curd/src/test/java/org/exitsoft/showcase/test/manager/ManagerTestCaseSupport.java
@@ -11,6 +11,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -20,8 +21,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * @author vincent
  *
  */
+@ActiveProfiles("test")
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("/applicationContext-core-test.xml")
+@ContextConfiguration("/applicationContext-core.xml")
 public class ManagerTestCaseSupport {
 	
 	private DataSource dataSource;
